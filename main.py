b611d9cc6cf7e47be40fccf4a0165a52a9bc1cc2
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-10-29 13:50:25 -0400

    fixed comp choice logic inclusive of only int 0-2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import ascii
-import computer_choice
+import computer_choice_logic
 
-print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+#print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+user_choice = input()
+computer_choice = computer_choice_logic.computer_choice
 
